9a33e6f67cc957b7235667fd21f15f1302205797
Nataliya Ioffe <nataliya.ioffe@rangle.io>
2022-01-12 17:33:55 -0500

    Integrate Keystone CMS into the backend and create data types

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,66 @@
+import 'dotenv/config';
+
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User', // Which schema is responsible for being the user? (Customer, Person?)
+  identityField: 'email', // Which field in "User" is going to ID the user? (what do they login with?)
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    //   TODO: Add in initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      //   TODO: add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the database');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      // Schema items go here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // Show UI to users who pass this test
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: 'id',
+    }),
+  })
+);
