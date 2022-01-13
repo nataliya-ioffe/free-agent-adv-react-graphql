11f51a2b456a2e535d0a3a54b59a173cf2f2f2bf
Nataliya Ioffe <nataliya.ioffe@rangle.io>
2022-01-13 17:33:52 -0500

    Add nav styles

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -29,8 +29,6 @@ const ProductsListStyles = styled.div`
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
 
-  console.log(data, error, loading);
-
   if (loading) {
     return <p>LOADING...</p>;
   }
