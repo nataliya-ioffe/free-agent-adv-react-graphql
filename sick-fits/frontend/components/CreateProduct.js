0ab35957e5959fc93d25498f48de922c1c20b2b6
Nataliya Ioffe <nataliya.ioffe@rangle.io>
2022-01-14 18:29:42 -0500

    Add CREATE_PRODUCT_MUTATION

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,16 +1,61 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './DisplayError';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    #   which variables are getting passed in? What types are they?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        # In our case, the Product Image is a different data type with a -relationship- to the Product.
+        # Here we are creating a ProductImage with the passed in image and description, and making a relationship between it and the Product
+        photo: { create: { image: $image, altText: $description } }
+      }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm();
 
+  const [createProduct, { data, error, loading }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
+
+        // Submit the input fields to the backend via GraphQL:
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      {/* The error component returns null if no error */}
+      <DisplayError error={error} />
+
+      {/* Show loading animation and disable inputs if its loading */}
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           <input
             required
@@ -34,7 +79,7 @@ export default function CreateProduct() {
 
         <label htmlFor="number">
           <input
-            type="text"
+            type="number"
             id="price"
             name="price"
             placeholder="price"
